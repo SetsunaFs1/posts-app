b8e8412f70ff2990cb006fb0a712f5904f55a6da
Angelina <Setsuna.FS@yandex.ru>
2025-07-06 01:08:23 +0300

    исправления

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -1,13 +1,12 @@
-import Footer from "../../widgets/LayoutFooter/Footer";
-import Header from "../../widgets/LayoutHeader/Header";
-import PostList from "../../widgets/PostList/PostList";
-
-export default function MainLayout() {
-  return (
-    <>
-      <Header />
-      <PostList />
-      <Footer />
-    </>
-  );
-}
+ import Footer from "../../widgets/LayoutFooter/Footer";
+ import Header from "../../widgets/LayoutHeader/Header";
+import type { MainLayoutProps } from "./MainLayoutType";
+ 
+export default function MainLayout({ children }: MainLayoutProps) {
+   return (
+     <>
+       <Header />
+      <main>{children}</main>
+       <Footer />
+     </>
+   )}
