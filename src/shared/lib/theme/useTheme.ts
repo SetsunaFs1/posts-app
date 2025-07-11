e66826fe02ddfc78a69320fc0689dbd9ac40920a
Angelina <Setsuna.FS@yandex.ru>
2025-07-11 23:33:09 +0300

    исправление замечаний

--- src/shared/lib/theme/useTheme.ts
+++ src/shared/lib/theme/useTheme.ts
@@ -3,4 +3,8 @@ import type { ThemeContextType } from "./themeContextType";
 
 export const ThemeContext = React.createContext<ThemeContextType | null>(null);
 
-export const useTheme = () => useContext(ThemeContext) as ThemeContextType;
+export const useTheme = () => {
+  const context = useContext(ThemeContext) as ThemeContextType;
+  if (!context) console.error("Error Context");
+  return context;
+};
