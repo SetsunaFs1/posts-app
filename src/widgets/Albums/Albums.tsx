ed83d8209cee8b8f357ab2d1e5f83c838a48d9c3
Angelina <Setsuna.FS@yandex.ru>
2025-07-23 12:32:25 +0300

    исправление замечаний

--- src/widgets/Albums/Albums.tsx
+++ src/widgets/Albums/Albums.tsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom';
 import { albums as data } from '../../mocks/albums';
 import styles from './Albums.module.css';
 import { useTheme } from '../../shared/lib/theme/useTheme';
+import React from 'react';
 
 export default function Albums() {
     const { theme } = useTheme();
@@ -11,25 +12,27 @@ export default function Albums() {
     return (
         <div className={styles.box}>
             <div className={styles.albums}>
-                <Link
-                    to={`/albums/${id}/photos`}
-                    className={`${
-                        theme === 'light'
-                            ? `${styles.linkLight}`
-                            : `${styles.linkDark}`
-                    } ${styles.link}`}
-                >
-                    {albums.map((album) => {
-                        return (
-                            <div key={album.id} className={styles.flex}>
-                                <div className={styles.albumElement}></div>
-                                <p className={styles.albumName}>
-                                    {album.title}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </Link>
+                {albums.map((album) => {
+                    return (
+                        <React.Fragment key={album.id}>
+                            <Link
+                                to={`/albums/${album.id}/photos`}
+                                className={`${
+                                    theme === 'light'
+                                        ? `${styles.linkLight}`
+                                        : `${styles.linkDark}`
+                                } ${styles.link}`}
+                            >
+                                <div key={album.id} className={styles.flex}>
+                                    <div className={styles.albumElement}></div>
+                                    <p className={styles.albumName}>
+                                        {album.title}
+                                    </p>
+                                </div>
+                            </Link>
+                        </React.Fragment>
+                    );
+                })}
             </div>
         </div>
     );
--- src/widgets/Photos/Photos.tsx
+++ src/widgets/Photos/Photos.tsx
@@ -7,7 +7,8 @@ export default function Photos() {
     const { theme } = useTheme();
     const navigate = useNavigate();
     const { id } = useParams();
-    const filterPhotos = photos.filter((item) => item.authorId === id);
+    const filterPhotos = photos.filter((item) => item.albumId === id);
+    console.log(filterPhotos);
 
     return (
         <div className={styles.box}>
